316b10bd1b483f33529da4f50656964f5ccb8268
zishone <zishgarces@gmail.com>
2021-01-01 11:11:17 +0100

    Fix jsend error type

--- src/types/express/index.ts
+++ src/types/express/index.ts
@@ -6,7 +6,7 @@ declare global {
       jsend: {
         success: (data: any, statusCode?: number) => void;
         fail: (data: any, statusCode?: number) => void;
-        error: (message: string, code?: number, data?: any, statusCode?: number) => void;
+        error: (message: string, statusCode?: number, code?: number, data?: any) => void;
       };
     }
   }
